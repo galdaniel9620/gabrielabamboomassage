6cb3ae0a965a4ee77ac8dd8059c0cdd78351a548
Daniel Gal <47724705+galdaniel9620@users.noreply.github.com>
2021-03-08 22:04:01 +0200

    1

---
+++ home.js
@@ -0,0 +1,27 @@
+AOS.init();
+
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.navLinks')
+    const navLinks = document.querySelectorAll('.navLinks li a')
+
+    burger.addEventListener('click' , () => {
+        nav.classList.toggle('nav-active')
+
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation = ''
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`
+            }
+        })
+
+        burger.classList.toggle('toggle')
+    })
+
+
+}
+
+navSlide()
+
